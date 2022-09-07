381ce6d2995eb528b77e6317f35600e9aea79d8a
Matias5598 <matiasf43@gmail.com>
2022-09-07 14:11:04 -0300

    Programa funcional al 100%

--- logica-encriptador.js
+++ logica-encriptador.js
@@ -9,21 +9,18 @@ function validarIngreso(texto){
 };
 
 function mostrarResultado(resultado){
-    var mostrar=resultado
-    
-    visibilidadElementos();
-    
+    var mostrar=resultado;
     document.getElementById("resultado").innerHTML = mostrar;
     
 };
 
 function visibilidadElementos(booleano){
-    var ocultar = booleano
-    if (ocultar){
-        document.getElementById("imagen-muñeco").style.visibility="hidden";
-        document.getElementById("caja-info-resultados").style.visibility="hidden";
+    if (booleano){
+        document.getElementById("resultado-1").style.visibility="hidden";
+        document.getElementById("resultado-2").style.visibility="visible";
     } else {
-        document.getElementById("texto-resultado").style.visibility="hidden";
+        document.getElementById("resultado-1").style.visibility="visible";
+        document.getElementById("resultado-2").style.visibility="hidden";
     };
 
 };
@@ -31,7 +28,7 @@ function visibilidadElementos(booleano){
 function encriptar (){
     var texto=document.getElementById("campo-texto").value;
     var validacion = validarIngreso(texto);
-    
+    visibilidadElementos(validacion)
     if (validacion){
         var mensaje = ''
         for(var posicion=0; posicion<texto.length; posicion++ ){
@@ -61,21 +58,64 @@ function encriptar (){
         }
 
     mostrarResultado(mensaje);
-    
-    }else{
-        
     };
 
 };
 
 function desencriptar(){
+    var texto=document.getElementById("campo-texto").value;
+    var validacion = validarIngreso(texto);
+    visibilidadElementos(validacion);
+
+    if(validacion){
+        var mensaje = '';
+        for(var posicion=0; posicion<texto.length; posicion++ ){
+            var letra=texto[posicion];
+            
+            if(letra=="a"){
+                mensaje+='a';
+                posicion+=1;
+
+            }else if(letra=='e'){
+                mensaje+='e';
+                posicion+=4;
+
+            }else if( letra=='i'){
+                mensaje+='i';
+                posicion+=3;
+
+            }else if(letra=='o'){
+                mensaje+='o';
+                posicion+=3;
+
+            }else if (letra=='u'){
+                mensaje+='u';
+                posicion+=3;
+
+            } else if(letra!='a','e','i','o','u') {
+                mensaje+=letra;
+
+            } else{
+                mensaje+='';
+            };
+        };
+
     
+    };
+    mostrarResultado(mensaje);
 };
 
 
+function copiar(){
+    var texto = document.getElementById("resultado").value;
+    navigator.clipboard.writeText(texto);
+};
 
 var botonEncriptar = document.getElementById("boton-encriptar");
 var botonDesencriptar = document.getElementById("boton-desencriptar");
+var botonCopiar = document.getElementById("boton-copiar");
 
 botonEncriptar.onclick= function() {encriptar()};
-botonDesencriptar.onclick= function() {validarIngreso()};
+botonDesencriptar.onclick= function() {desencriptar()};
+botonCopiar.onclick = function() {copiar()};
+
